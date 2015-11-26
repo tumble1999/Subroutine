bf9414d79aeae6118685a0edc66b343b04884768
tumble1999 <tumble@tumblegamer.tk>
2015-11-26 15:37:12 +0000

    Added Initial Files

---
+++ Subroutines/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Subroutines
+{
+    class Program
+    {
+        public static void greet()
+        {
+            Console.WriteLine("Help!, I'm being forced to print text to the screen.");
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("string[] args = " + args);
+            Console.WriteLine();
+            greet();
+     
+            // Program.Main(args); DO NOT UNCOMMENT
+            Console.ReadKey();
+        }
+    }
+}
